82890747c921b93ba13696d405dc0b385dba247e
YoungWolf <muzunov875@gmail.com>
2017-12-02 16:00:42 +0200

    Implemented elevation method

--- BattleTanks/Source/BattleTanks/Private/TankBarrel.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankBarrel.cpp
@@ -1,10 +1,18 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankBarrel.h"
+#include "Engine/World.h"
 
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
+	RelativeSpeed = FMath::Clamp(RelativeSpeed, -1.0f, 1.0f);
 
+	float ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	float RawElevation = RelativeRotation.Pitch + ElevationChange;
+
+	float Elevation = FMath::Clamp(RawElevation, MinElevationDegrees, MaxElevationDegrees);
+
+	SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
 
 

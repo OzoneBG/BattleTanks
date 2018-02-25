3a3d3c9d09e782be7a3670cebf46587685a50013
YoungWolf <muzunov875@gmail.com>
2018-02-25 16:43:41 +0200

    added another layer for controlling the tank for both AI and player

---
+++ BattleTanks/Source/BattleTanks/Private/TankMovementComponent.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankMovementComponent.h"
+
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
+
+void UTankMovementComponent::IntendMoveForward(float Throw) 
+{
+	if (!LeftTrack || !RightTrack) return;
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+}
+
+void UTankMovementComponent::IntendMoveRight(float Throw)
+{
+	if (!LeftTrack || !RightTrack) return;
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
+}
+
+

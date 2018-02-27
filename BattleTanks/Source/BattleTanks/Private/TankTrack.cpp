e520b110c3860b76287874992f3b203b50db76e9
YoungWolf <muzunov875@gmail.com>
2018-02-27 23:58:13 +0200

    Improvements to tank controls

--- BattleTanks/Source/BattleTanks/Private/TankTrack.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankTrack.cpp
@@ -2,14 +2,19 @@
 
 #include "TankTrack.h"
 
-UTankTrack::UTankTrack()
+void UTankTrack::BeginPlay()
 {
-	PrimaryComponentTick.bCanEverTick = true;
+	Super::BeginPlay();
+
+	//Overlap hit method
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
 }
 
-void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UTankTrack::ApplySidewaysForce()
 {
 	float SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+
+	float DeltaTime = GetWorld()->GetDeltaSeconds();
 	FVector CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
 
 	UStaticMeshComponent* TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
@@ -19,12 +24,41 @@ void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActor
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-	//Clamp throttle value so the player cannot over-drive
+	CurrentThrottle = FMath::Clamp<float>(CurrentThrottle + Throttle, -1, 1);
+}
 
-	FVector ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+void UTankTrack::DriveTrack()
+{
+	UE_LOG(LogTemp, Warning, TEXT("%s: %s throttle at %f"), *GetOwner()->GetName(), *GetName(), CurrentThrottle);
+
+	FVector ForceApplied = GetForwardVector() * CurrentThrottle * TrackMaxDrivingForce;
 	FVector ForceLocation = GetComponentLocation();
 	UPrimitiveComponent* TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
 
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (!IsAboveSpeed())
+	{
+		DriveTrack();
 
+		ApplySidewaysForce();
+	}
+	CurrentThrottle = 0;
+}
+
+bool UTankTrack::IsAboveSpeed()
+{
+	FVector Velocity = GetOwner()->GetVelocity();
+	float Speed = Velocity.Size();
+
+	if (Speed > MaxSpeed)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
--- BattleTanks/Source/BattleTanks/Public/TankTrack.h
+++ BattleTanks/Source/BattleTanks/Public/TankTrack.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/StaticMeshComponent.h"
+#include "Engine/World.h"
 #include "TankTrack.generated.h"
 
 /**
@@ -15,9 +16,7 @@ class BATTLETANKS_API UTankTrack : public UStaticMeshComponent
 	GENERATED_BODY()
 	
 public:
-	UTankTrack();
-
-	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void BeginPlay() override;
 
 	// Sets throttle between -1 and +1
 	UFUNCTION(BlueprintCallable, Category = Input)
@@ -25,7 +24,20 @@ public:
 
 	// Max force per track, in Newtons
 	UPROPERTY(EditDefaultsOnly)
-	float TrackMaxDrivingForce = 400000;
+	float TrackMaxDrivingForce = 400000; //400 000
 	
-	
+private:
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
+	UPROPERTY(EditDefaultsOnly)
+	float MaxSpeed = 1200;
+
+	void DriveTrack();
+
+	void ApplySidewaysForce();
+
+	bool IsAboveSpeed();
+
+	float CurrentThrottle = 0;
 };
